e558a76da91303c5754b4e29b88f719607c644e4
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-31 20:53:49 -0700

    Fix styling and grid position on page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector('.grid-container');
+const root = document.querySelector(':root');
 let gridSize = 16;
 
 function createGrid(gridSize) {
+    root.style.setProperty('--grid-size', gridSize.toString());
     for (let i=0; i < gridSize**2; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
