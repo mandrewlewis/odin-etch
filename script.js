2428c2ab7578b136558ede104f26912b084fefb4
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-31 19:00:06 -0700

    Fix grid generation (squared gridSize value)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gridContainer = document.querySelector('.grid-container');
 let gridSize = 16;
 
 function createGrid(gridSize) {
-    for (let i=0; i < gridSize; i++) {
+    for (let i=0; i < gridSize**2; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         gridContainer.appendChild(square);
