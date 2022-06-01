1309bd7c3a07ea85743135a9138367cc7dd56509
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-31 22:03:37 -0700

    Add mouseover event to grid and newGrid function

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const gridContainer = document.querySelector('.grid-container');
 const root = document.querySelector(':root');
 let gridSize = 16;
+let squares = [];
 
-function createGrid(gridSize) {
+
+function createGrid() {
+    // Generate and append nodes
     root.style.setProperty('--grid-size', gridSize.toString());
     for (let i=0; i < gridSize**2; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         gridContainer.appendChild(square);
     }
+
+    // Add event listeners
+    squares = document.querySelectorAll('.square');
+    squares.forEach(sq => {
+        sq.addEventListener('mouseover', () => {
+            sq.classList.add('hover');
+        });
+    })
 }
+
+
+function newGrid() {
+    let input = Number(prompt('What size grid?\n\n(Enter 1 integer between 1 and 100)'));
+    if (input) {
+        gridSize = Math.floor(input);
+    } else { return; }
     
-createGrid(gridSize);
+    squares.forEach(sq => { gridContainer.removeChild(sq); });
+    if (gridSize > 100) { gridSize = 100; } 
+
+    createGrid();
+}
+
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', newGrid);
+
+
+createGrid();
